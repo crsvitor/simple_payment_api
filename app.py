8a750467037967e28581775c91526c6f3a3f20d4
crsvitor <dev.castrors@gmail.com>
2024-09-15 22:52:27 -0300

    feat: add websocket connection

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,6 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'in
 app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_pix_payment():
@@ -61,5 +63,10 @@ def payment_pix_page(payment_id):
         qr_code=payment.qr_code
     )
 
+#websocket
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected to the server')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
